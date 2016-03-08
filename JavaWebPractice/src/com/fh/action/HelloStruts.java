f63f14960867cd101a236a9cdb32a36167d2744c
hao19 <hao199209@gmail.com>
2016-03-08 23:18:24 +0800

    aaa

---
+++ JavaWebPractice/src/com/fh/action/HelloStruts.java
@@ -0,0 +1,13 @@
+package com.fh.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class HelloStruts implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		
+		return Action.SUCCESS;
+	}
+
+}
